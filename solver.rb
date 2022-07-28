22419a5713b636861005968a1c7f0e9a8ea6fac0
Rafael Guzman <rega1237@gmail.com>
2022-07-28 16:36:27 -0400

    Create another test case for factorial method and write the code for class solver and factorial method

---
+++ solver.rb
@@ -0,0 +1,15 @@
+class Solver
+  def factorial(factorial_number)
+    fact = 1
+    if (factorial_number == 0)
+      fact
+    else
+      i = 1
+      while(i <= factorial_number)
+        fact = fact * i
+        i += 1    
+      end
+    end
+    fact
+  end
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,8 +1,15 @@
+require_relative '../solver'
+
 describe Solver do
   context 'When test Solver class' do
-    it 'Pass a number and return the factorial result' do
+    it 'Pass a number and return the correct factorial result' do
       solver = Solver.new()
       expect(solver.factorial(5)).to eq 120
+    end
+
+    it 'Pass a number and return the wrong factorial result' do
+      solver = Solver.new()
+      expect(solver.factorial(6)).not_to eq 600
     end
   end
 end
