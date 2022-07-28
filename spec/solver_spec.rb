771ebb16a1110ef6e98e6dc5135bddfd2c3776c2
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-07-28 22:45:14 +0200

    Add test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../solver'
 
 describe Solver do
-  context 'When test Solver class' do
+  context 'When test factorial method' do
     it 'Pass a number and return the correct factorial result' do
       solver = Solver.new()
       expect(solver.factorial(5)).to eq 120
@@ -10,6 +10,13 @@ describe Solver do
     it 'Pass a number and return the wrong factorial result' do
       solver = Solver.new()
       expect(solver.factorial(6)).not_to eq 600
+    end
+  end
+
+  context 'When test reverse method' do
+    it 'takes a string and return a the reversed string' do
+      solver = Solver.new()
+      expect(solver.reverse("hello")).to eq "olleh"
     end
   end
 end
